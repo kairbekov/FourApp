743db10a34e66c949fbd4b66a3d478df21046740
kairbekov <abuka93@gmail.com>
2017-02-04 08:45:55 +0600

    Update Const.java

--- app/src/main/java/kz/strixit/abuka/fourapp/Const.java
+++ app/src/main/java/kz/strixit/abuka/fourapp/Const.java
@@ -4,7 +4,7 @@ package kz.strixit.abuka.fourapp;
  * Created by abuka on 14.07.2016.
  */
 public class Const {
-    public static final String APP_ID = "C34B1160-D4A0-1185-FFF3-1E3D4EE0BC00";
-    public static final String ANDROID_KEY = "072B0E5E-1169-C9E1-FF58-9AE4DAAC3300";
+    public static final String APP_ID = "";
+    public static final String ANDROID_KEY = "";
     public static final String VERSION = "v1";
 }
